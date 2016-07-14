0c0b1eb6a00bceafbcbe4b1cd3020d31f91a8c92
albert_kuo <albert_kuo@cht.com.tw>
2016-07-14 15:31:31 +0800

    Hello World Example

---
+++ HelloWorld/helloWorld.js
@@ -0,0 +1,21 @@
+/* The DOMContentLoaded event is fired when the initial HTML document 
+ * has been completely loaded and parsed, without waiting for stylesheets, 
+ * images, and subframes to finish loading.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var searchBtn = document.getElementById('doSearch'); 
+
+    // Execute the following steps as user click Search button       
+    searchBtn.addEventListener('click', function(){      
+        // Gets query string 
+        var queryString = document.getElementById('queryString').value;
+
+        // Create URL with query string
+        var newUrl = 'https://www.flickr.com/search/?q='+queryString;
+
+        // Creates a new tab.
+        chrome.tabs.create({ url : newUrl});
+    }, false);
+
+}, false);    
