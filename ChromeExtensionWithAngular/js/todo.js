5fa194f584e749429368da8a713e1a4f8065d2ee
albert_kuo <albert_kuo@cht.com.tw>
2016-07-15 10:58:37 +0800

    Create a simple chrome extenstion with AngularJS 1.X

---
+++ ChromeExtensionWithAngular/js/todo.js
@@ -0,0 +1,24 @@
+angular.module('todoApp', [])
+    .controller('todoController', function($scope) {
+
+        $scope.tasks = [];
+        
+        $scope.addTask = function() {
+            if(isNotEmptyOrUndefined($scope.task)){
+                 $scope.tasks.push($scope.task);
+                $scope.task = '';
+            } else {
+                alert('task name cannot be null');
+            }           
+        }
+        
+        $scope.deleteTask = function() {
+            $scope.tasks.splice(this.$index, 1);
+        }
+
+});
+
+// 檢查是串是否不為空值且不為undefined
+function isNotEmptyOrUndefined(str) {
+    return str != '' && !angular.isUndefined(str) && str != null;
+}
